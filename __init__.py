90029e3310146b825291189c44e01b977eda508a
Danila Korytov <dkorytov@uchicago.edu>
2019-03-18 13:44:30 -0500

    init commit

---
+++ __init__.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+
+from .param import Param, CosmoParam
+from .cosmo import *
+from .catalog_reader import *
+from .sort import *
+try:
+    from .power_spectrum import *
+    from .util import *
+    from .contour import *
+except ImportError as ie:
+    print("Could not import util.py and contour.py:",ie.args)
+try:
+    from .gio import *
+except OSError as ie:
+    print("Could not import gio.py due to dtk/libs/libpygio.so not being built. Skipping")
+    print("\tRead dtk/pygio/README.TXT on how to compile libpygi.so")
+# from .gio import *
